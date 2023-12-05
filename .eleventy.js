fd18530df5eea54ba525796e913162fbfcb00a50
Wing Pang <wingy.wing@icloud.com>
2023-12-05 23:55:52 +0900

    fix some passthrough stuff

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,10 @@ const eleventySass = require("eleventy-sass");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(eleventySass); //to add SASS support from src
-    eleventyConfig.addPassthroughCopy({"node_modules/gsap/dist/all.js": "src/assets"}); //to add GSAP
+    eleventyConfig.addPassthroughCopy({"node_modules/gsap/dist/all.js":"/assets"}); //to add GSAP
+    eleventyConfig.addPassthroughCopy({"src/assets":"/assets"});
     eleventyConfig.setBrowserSyncConfig({
         files: './dist/**/*'
-    })
+    });
     return { dir: { input: "src", output: "dist" } };
 };
