92c97dc78013cdf7f6be62bfdaea6b20fcf138e1
Wing Pang <wingy.wing@icloud.com>
2023-12-07 01:04:05 +0900

    Couldn't get webc to work with sass bundling so rollbacked changes

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,22 @@
 const eleventySass = require("eleventy-sass");
+const pluginWebc = require("@11ty/eleventy-plugin-webc");
+const { EleventyRenderPlugin } = require("@11ty/eleventy");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(eleventySass); //to add SASS support from src
+    eleventyConfig.addPlugin(EleventyRenderPlugin);
+    eleventyConfig.addPlugin(pluginWebc, {
+        components: "src/_includes/components/**/*.webc"
+    })
     eleventyConfig.addPassthroughCopy("node_modules/gsap/dist/all.js"); //to add GSAP
     eleventyConfig.addPassthroughCopy("src/assets");
+    eleventyConfig.addPassthroughCopy("src/wenc-setup.js");
+    eleventyConfig.setTemplateFormats(["njk"]);
     return { 
         dir: { 
             input: "src", 
             output: "dist", 
-            includes: "_includes"
+            includes: "_includes",
+            layouts: "_includes/layouts"
         } };
 };
