1700ad800aa7105adad6e6e3839a986ca2bf29e5
Wing Pang <wingy.wing@icloud.com>
2023-12-07 00:48:59 +0900

    stuff

--- .eleventy.js
+++ .eleventy.js
@@ -4,5 +4,10 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(eleventySass); //to add SASS support from src
     eleventyConfig.addPassthroughCopy("node_modules/gsap/dist/all.js"); //to add GSAP
     eleventyConfig.addPassthroughCopy("src/assets");
-    return { dir: { input: "src", output: "dist" } };
+    return { 
+        dir: { 
+            input: "src", 
+            output: "dist", 
+            includes: "_includes"
+        } };
 };
