e5adcf622f8be13250bd71cfbf91e2111ebcc5bf
Wing Pang <wingy.wing@icloud.com>
2023-12-09 23:55:23 +0900

    Mobile menu open + closes now

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,13 @@
 const eleventySass = require("eleventy-sass");
-const pluginWebc = require("@11ty/eleventy-plugin-webc");
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(eleventySass); //to add SASS support from src
     eleventyConfig.addPlugin(EleventyRenderPlugin);
-    eleventyConfig.addPlugin(pluginWebc, {
-        components: "src/_includes/components/**/*.webc"
-    })
     eleventyConfig.addPassthroughCopy("node_modules/gsap/dist/all.js"); //to add GSAP
+    eleventyConfig.addPassthroughCopy("node_modules/jquery/dist/jquery.min.js"); //to add jquery
+    eleventyConfig.addPassthroughCopy("src/js"); //to add jquery
+
     eleventyConfig.addPassthroughCopy("src/assets");
     eleventyConfig.addPassthroughCopy("src/wenc-setup.js");
     eleventyConfig.setTemplateFormats(["njk"]);
