a46e2eb4adbeb4591f6872c34c119ca76a984d2d
Wing Pang <wingy.wing@icloud.com>
2024-02-29 20:10:08 +1100

    jump to section

--- .eleventy.js
+++ .eleventy.js
@@ -2,12 +2,14 @@ const eleventySass = require("eleventy-sass");
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
 const { eleventyImagePlugin } = require("@11ty/eleventy-img");
 const implicitFigures = require('markdown-it-image-figures');
+const mdAnchor = require('markdown-it-anchor');
 
 
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPlugin(eleventySass); //to add SASS support from src
     eleventyConfig.addPlugin(EleventyRenderPlugin);
+    eleventyConfig.addWatchTarget('src/projects/**/**.md');
     eleventyConfig.addPassthroughCopy("node_modules/gsap/dist/all.js"); //to add GSAP
     eleventyConfig.addPassthroughCopy("node_modules/jquery/dist/jquery.min.js"); //to add jquery
     eleventyConfig.addPassthroughCopy("src/js"); //to add jquery
@@ -18,8 +20,9 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/projects/**/**.mp4");
     eleventyConfig.setTemplateFormats(["njk,md"]);
     eleventyConfig.amendLibrary("md", mdLib => mdLib.use(implicitFigures, {
-      figcaption: "alt"
+      figcaption: "title"
     }));
+    eleventyConfig.amendLibrary("md", mdLib => mdLib.use(mdAnchor));
     eleventyConfig.addLayoutAlias('project', 'layouts/project/project.njk');
     eleventyConfig.addPlugin(eleventyImagePlugin, {
       // Set global default options
