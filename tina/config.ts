98b8c23e56f469fbbd0855549db6ce42a68d36ea
Wing Pang <wingy.wing@icloud.com>
2024-05-18 03:10:17 +1000

    updated media folder

--- tina/config.ts
+++ tina/config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   media: {
     tina: {
       mediaRoot: "assets/img",
-      publicFolder: "dist",
+      publicFolder: "src",
     },
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
