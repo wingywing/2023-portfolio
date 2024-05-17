b81505b6413843c96e87af7a2941d74a050ed349
Wing Pang <wingy.wing@icloud.com>
2024-05-17 23:31:54 +1000

    feat(*): added notes

---
+++ _data/webmention.js
@@ -0,0 +1,10 @@
+const { defaults } = require("@chrisburnell/eleventy-cache-webmentions")
+
+// Load .env variables with dotenv
+require("dotenv").config()
+
+module.exports = Object.assign({}, defaults, {
+	domain: "https://wingpang.com",
+	feed: `https://webmention.io/api/mentions.jf2?domain=wingpang.com&token=${process.env.WEBMENTION_IO_TOKEN}&per-page=9001`,
+	key: "children",
+})
