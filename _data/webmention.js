edbc14d7788713e19f68697b625bafd1346ad726
Wing Pang <wingy.wing@icloud.com>
2024-05-18 12:07:22 +1000

    fix(*): webmention token

--- _data/webmention.js
+++ _data/webmention.js
@@ -5,6 +5,6 @@ require("dotenv").config()
 
 module.exports = Object.assign({}, defaults, {
 	domain: "https://wingpang.com",
-	feed: `https://webmention.io/api/mentions.jf2?domain=wingpang.com&token=t-ACUe-_n50x0DTa3R-EXQ&per-page=9001`,
+	feed: `https://webmention.io/api/mentions.jf2?domain=wingpang.com&token=WEBMENTION_IO_TOKEN&per-page=9001`,
 	key: "children",
 })
