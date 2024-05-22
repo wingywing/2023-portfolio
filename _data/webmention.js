901a34158ee5a18d37b2e3f7a7b1a0e46caa2a5e
Wing Pang <wingy.wing@icloud.com>
2024-05-23 00:15:02 +1000

    fix(*): tokens from context.env -> process.env

--- _data/webmention.js
+++ _data/webmention.js
@@ -5,6 +5,6 @@ require("dotenv").config()
 
 module.exports = Object.assign({}, defaults, {
 	domain: "https://wingpang.com",
-	feed: `https://webmention.io/api/mentions.jf2?domain=wingpang.com&token=context.env.WEBMENTION_IO_TOKEN&per-page=9001`,
+	feed: `https://webmention.io/api/mentions.jf2?domain=wingpang.com&token=process.env.WEBMENTION_IO_TOKEN&per-page=9001`,
 	key: "children",
 })
